b3d2b22eb8ce3f4d7fe9eec2d86ab89685689c5f
akaguny <alexeykolpino@gmail.com>
2017-07-09 14:18:28 +0300

    chore(*): функционал для работы с отчётами eslint вынесен в модуль

--- index.js
+++ index.js
@@ -9,163 +9,77 @@
 'use-strict';
 const fs = require('fs-extra'),
       path = require('path'),
-      isEqual = require('lodash.isequal'),
-      clone = require('lodash.clone'),
-      procArg = process.argv;
-let currentJSON,
-    masterJSON,
-    resultJSONPath;
+      procArg = process.argv,
+      allowedModes = {
+        eslint: 'eslint'
+      };
 
 if (require.main === module) {
-  currentJSON = fs.readJSON(`${procArg[procArg.indexOf('-current') + 1]}`);
-  masterJSON = fs.readJSON(`${procArg[procArg.indexOf('-master') + 1]}`);
-  resultJSONPath = path.resolve(path.dirname(procArg[1]), `result.json`);
-  main(masterJSON, currentJSON);
+  main(procArg.slice(1));
 } else {
   module.exports = main;
 }
 
-// FIXME: убрать за флаг, т.к. используется только для отладки
-console.log(`${procArg[procArg.indexOf('-current') + 1]}`);
-console.log(`${procArg[procArg.indexOf('-master') + 1]}`);
-console.log(resultJSONPath);
+function main (args) {
+  let currentMode = getCurrentMode(args);
 
-function main (masterJSON, currentJSON) {
-  return Promise.all([masterJSON, currentJSON]).then((resultObjectArrays) => {
-    return {
-      master: customEslintResultsIterator(resultObjectArrays[0]),
-      current: customEslintResultsIterator(resultObjectArrays[1])
-    };
-  }).then((resultMapsObject) => {
-    return getUnicalNewErrors(resultMapsObject.master, resultMapsObject.current);
-  }).then((unicalErrors) => {
-    writeResult(unicalErrors, resultJSONPath);
-  });
+  runChecks(currentMode, args);
 };
 
 /**
- * Получение результатов проверки с уникальными сообщений об ошибках
- * @param {LintResult} lintResultMaster - мастер
- * @param {LintResult} lintResultCurrent - текущая
- * @return {LintResult} рузультат проверки с уникальными ошибками
+ * Определить текущий режим работы
+ * @param {Array} - mainArgs массив аргументов коммандной
+ * @return {String} - режим
  */
-function getLintResultWithUnicalNewMessages (lintResultMaster, lintResultCurrent) {
-  let unicalNewMessages = clone(lintResultCurrent),
-      result,
-      warningCount = 0,
-      errorCount = 0;
+function getCurrentMode (mainArgs) {
+  let currentMode;
 
-  unicalNewMessages.messages = lintResultCurrent.messages.filter((item, i) => {
-    let elementExist;
-
-    elementExist = lintResultMaster.messages.some((masterItem) => {
-      return isEqual(item, masterItem);
-    });
-
-    return !elementExist;
-  }, 0).map((item) => {
-    item.severity.toString() === '1' ? warningCount++ : errorCount++;
-    return item;
-  });
-
-  unicalNewMessages.warningCount = warningCount;
-  unicalNewMessages.errorCount = errorCount;
-
-  if (unicalNewMessages.messages.length > 0) {
-    result = unicalNewMessages;
-  }
-  return result;
-}
-
-/**
- * Получить уникальные, новые ошибки. Старые берутся с master ветки
- * @param {Map.<EslintErrorFileName, LintResult[]>} errorsMapMaster ошибки ветки
- * @param {Map.<EslintErrorFileName, LintResult[]>} errorsMapCurrent ошибки текущей ветки
- * @return {LintResult[]} обработанные результаты проверок. Только уникальные
- */
-function getUnicalNewErrors (errorsMapMaster, errorsMapCurrent) {
-  let errorsMapMasterEmpty = errorsMapMaster.size === 0,
-      errorsMapCurrentEmpty = errorsMapCurrent.size === 0,
-      unicalNewErrors = new Map(),
-      lintResultWithUnicalNewMessages;
-
-  if (!errorsMapCurrentEmpty) {
-    if (errorsMapMasterEmpty) {
-      unicalNewErrors = [...errorsMapMasterEmpty.values()];
+  for (let mode in allowedModes) {
+    if (mainArgs.indexOf(mode) === -1) {
+      //TODO: вынести сообщения об ошибках в хелпер
+      throw new Error(`Поддерживаемый режим работы не передан
+       ${Object.keys(allowedModes)}`);
     } else {
-      errorsMapCurrent.forEach((value, key) => {
-        let masterHasErrorInFile = errorsMapMaster.has(key),
-            errorsInFileIsIden = isEqual(value, errorsMapMaster.get(key));
-
-        if (masterHasErrorInFile && !errorsInFileIsIden) {
-          lintResultWithUnicalNewMessages = getLintResultWithUnicalNewMessages(errorsMapMaster.get(key), value);
-          if (lintResultWithUnicalNewMessages) {
-            unicalNewErrors.set(key, lintResultWithUnicalNewMessages);
-          }
-        } else if (!errorsInFileIsIden) {
-          unicalNewErrors.set(key, value);
-        }
-      });
+      currentMode = mode;
+      break;
     }
+  };
+
+  return currentMode;
+};
+
+/**
+ * Запустить проверки
+ * @param {String} mode - режим
+ * @param {Aray} mainArgs - аргументы
+ */
+function runChecks (mode, mainArgs) {
+  switch (mode) {
+    case allowedModes.eslint:
+      require(path.resolve(__dirname, 'lib/eslint')).apply(null, prepareInput(mode, mainArgs));
   }
-  return [...unicalNewErrors.values()];
 }
 
 /**
- *
- * @param {LintResult} unicalErrors
- * @param {String} resultJSONPath
+ * Подготовить входные данные для режима
+ * @param {String} mode - режим
+ * @param {Aray} mainArgs - аргументы
+ * @return {Array} подготовленные входные данные
  */
-function writeResult (unicalErrors, resultJSONPath) {
-  fs.writeJSON(resultJSONPath, unicalErrors);
-}
+function prepareInput (mode, mainArgs) {
+  let currentJSON,
+      masterJSON,
+      resultJSONPath,
+      input;
 
-/**
- * Результаты проверок файлов линтером Eslint
- * @typedef {LintResult[]} EslintResults
- */
-
-/**
- * Имя файла, в котором найдены ошибки
- * @typedef {String} EslintErrorFileName
- */
-
-/**
- * A linting warning or error.
- * @typedef {Object} LintMessage
- * @property {string} message The message to display to the user.
- */
-
-/**
- * A linting result.
- * @typedef {Object} LintResult
- * @property {string} filePath The path to the file that was linted.
- * @property {LintMessage[]} messages All of the messages for the result.
- * @property {number} errorCount Number of errors for the result.
- * @property {number} warningCount Number of warnings for the result.
- * @property {number} fixableErrorCount Number of fixable errors for the result.
- * @property {number} fixableWarningCount Number of fixable warnings for the result.
- * @property {string=} [source] The source code of the file that was linted.
- * @property {string=} [output] The source code of the file that was linted, with as many fixes applied as possible.
- */
-
-/**
- * Итератор по результату eslint
- * @param {EslintResults} _resultObjectArray
- * @return {Map<filePath, LintResult>} объект с key - имя файла с
- * ошибкой
- */
-function customEslintResultsIterator (_resultObjectArray) {
-  let resultMap = new Map();
-
-  if (_resultObjectArray.length === 1 &&
-    Object.keys(_resultObjectArray[0]).length === 0) {
-    return resultMap;
+  switch (mode) {
+    case allowedModes.eslint:
+      currentJSON = fs.readJSON(`${mainArgs[mainArgs.indexOf('-current') + 1]}`);
+      masterJSON = fs.readJSON(`${mainArgs[mainArgs.indexOf('-master') + 1]}`);
+      resultJSONPath = path.resolve(path.dirname(mainArgs[1]), `result.json`);
+      //FIXME возвращать в виде объекта, использовать деструктуризацию
+      input = [masterJSON, currentJSON, resultJSONPath];
   }
 
-  _resultObjectArray.forEach((item) => {
-    resultMap.set(item.filePath, item);
-  });
-
-  return resultMap;
+  return input;
 }
