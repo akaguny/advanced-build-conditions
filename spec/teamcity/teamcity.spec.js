ab8794cf5ac2f04ac1a4b4f688a953027dd4a72c
akaguny <alexeykolpino@gmail.com>
2017-07-19 21:51:31 +0300

    test(*): заготовка тестов для модуля teamcity

--- spec/teamcity/teamcity.spec.js
+++ spec/teamcity/teamcity.spec.js
@@ -1,6 +1,7 @@
-const sh = require('shelljs'),
-      path = require('path'),
-      basePackagePath = path.resolve(__dirname, '..');
+const path = require('path'),
+      basePackagePath = path.resolve(__dirname, '..'),
+      nock = require('nock'),
+      teamcityHost = 'https://***REMOVED***';
 
 let tc;
 
@@ -9,15 +10,61 @@ describe('teamcity', () => {
     tc = require(path.resolve(basePackagePath, 'teamcity'));
   });
 
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
   describe('Модуль', () => {
+    const testBuildName = 'testBuildName',
+          testMasterBuildName = '1.12.0/develop',
+          testBuildStatus = 'Failed',
+          testBuildFailedReason = 'It`s not good build',
+          testBuildProblem = 'It`s real big problem';
+
     it('успешно подключен', () => {
       expect(tc).toBeDefined();
     });
+
     it('поддердивает необходимое api', () => {
       expect(tc.setBuildStatus).toBeDefined();
-      expect(tc.getMasterBuildArtifact).toBeDefined();
+
+      expect(tc.getBuildArtifact).toBeDefined();
+
       expect(tc.setBuildName).toBeDefined();
+
       expect(tc.getPullRequestNumber).toBeDefined();
+    });
+
+    describe('позволяет получать', () => {
+      it('артефакт мастер сборки', () => {
+        // https://confluence.jetbrains.com/display/TCD10/REST+API#RESTAPI-BuildArtifacts
+        nock(teamcityHost).get(/app\/rest\/builds\/.*\/artifacts\/content\/reports\.zip\/eslint.json/);
+        tc.getBuildArtifact(`${testMasterBuildName}`);
+
+        expect('buildArtifactURL').toEqual('anotherBuildArtifactURL');
+
+        expect(nock.pendingMocks()).toEqual([]);
+      });
+    });
+
+    describe('позволяет устанавливать', () => {
+      it('имя сборки', () => {
+        tc.setBuildName(`${testBuildName}`);
+
+        expect(process.stdout).toContain(`anyIsn't contain in stdout, ${testBuildName}`);
+      });
+
+      it('проблему сборки', () => {
+        tc.setBuildProblem(testBuildProblem);
+
+        expect(process.stdout).toContain(`##teamcity[buildProblem desтзьcription='${testBuildProblem}' identity='']`);
+      });
+
+      xit('статус сборки', () => {
+        tc.setBuildStatus(`${testBuildStatus.failed}`, `${testBuildFailedReason}`);
+
+        expect(process.stdout).toContain(`##teamcity[buildStatus status='${testBuildStatus.failed}' text='${testBuildFailedReason}']`);
+      });
     });
   });
 });
