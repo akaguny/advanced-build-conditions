f96652dc5caf9e3a61f95beefb5441b79639c768
Alexey Shcherbakov <Alexey.Shchbakov@billing.ru>
2017-07-28 18:22:58 +0300

    test(teamcity): правки тестов в связи с разработкой

--- spec/teamcity/teamcity.spec.js
+++ spec/teamcity/teamcity.spec.js
@@ -19,57 +19,63 @@ describe('teamcity', () => {
           testMasterBuildName = '1.12.0/develop',
           testBuildStatus = 'Failed',
           testBuildFailedReason = 'It`s not good build',
-          testBuildProblem = 'It`s real big problem';
+          testBuildProblem = 'It`s real big problem',
+          testUsername = 'teamcity',
+          testPassword = 'password',
+          testHost = 'localhost:8010',
+          testProjectId = 'testProjectId';
 
-    it('успешно подключен', () => {
+    it('подключен', () => {
       expect(tc).toBeDefined();
     });
 
     it('поддердивает необходимое api', () => {
-      expect(tc.setCreditials).toBeDefined();
+      expect(tc.init).toBeDefined();
 
       expect(tc.setBuildStatus).toBeDefined();
 
       expect(tc.getBuildArtifact).toBeDefined();
-
-      expect(tc.setBuildName).toBeDefined();
     });
 
-    describe('позволяет', () => {
+    describe('в работе', () => {
       beforeEach(() => {
-        tc.setCreditials('teamcity', 'password');
-      });
-    });
-
-    describe('позволяет получать', () => {
-      it('артефакт мастер сборки', () => {
-        // https://confluence.jetbrains.com/display/TCD10/REST+API#RESTAPI-BuildArtifacts
-        nock(teamcityHost).get(/app\/rest\/builds\/.*\/artifacts\/content\/reports\.zip\/eslint.json/);
-        tc.getBuildArtifact(`${testMasterBuildName}`);
-
-        expect('buildArtifactURL').toEqual('anotherBuildArtifactURL');
-
-        expect(nock.pendingMocks()).toEqual([]);
-      });
-    });
-
-    describe('позволяет устанавливать', () => {
-      it('имя сборки', () => {
-        tc.setBuildName(`${testBuildName}`);
-
-        expect(process.stdout).toContain(`anyIsn't contain in stdout, ${testBuildName}`);
+        tc.init({login: testUsername, pasword: testPassword, host: testHost, projectId: testProjectId},testMasterBuildName);
       });
 
-      it('проблему сборки', () => {
-        tc.setBuildProblem(testBuildProblem);
-
-        expect(process.stdout).toContain(`##teamcity[buildProblem desтзьcription='${testBuildProblem}' identity='']`);
+      it('использует входные данные', () => {
+        nock(teamcityHost)
       });
 
-      it('статус сборки', () => {
-        tc.setBuildStatus(`${testBuildStatus.failed}`, `${testBuildFailedReason}`);
+      describe('позволяет получать', () => {
+        it('артефакт мастер сборки', () => {
+          // https://confluence.jetbrains.com/display/TCD10/REST+API#RESTAPI-BuildArtifacts
+          nock(teamcityHost).get(/app\/rest\/builds\/.*\/artifacts\/content\/reports\.zip\/eslint.json/);
+          tc.getBuildArtifact(`${testMasterBuildName}`);
 
-        expect(process.stdout).toContain(`##teamcity[buildStatus status='${testBuildStatus.failed}' text='${testBuildFailedReason}']`);
+          expect('buildArtifactURL').toEqual('anotherBuildArtifactURL');
+
+          expect(nock.pendingMocks()).toEqual([]);
+        });
+      });
+
+      describe('позволяет устанавливать', () => {
+        it('имя сборки', () => {
+          tc.setBuildName(`${testBuildName}`);
+
+          expect(process.stdout).toContain(`anyIsn't contain in stdout, ${testBuildName}`);
+        });
+
+        it('проблему сборки', () => {
+          tc.setBuildProblem(testBuildProblem);
+
+          expect(process.stdout).toContain(`##teamcity[buildProblem desтзьcription='${testBuildProblem}' identity='']`);
+        });
+
+        it('статус сборки', () => {
+          tc.setBuildStatus(`${testBuildStatus.failed}`, `${testBuildFailedReason}`);
+
+          expect(process.stdout).toContain(`##teamcity[buildStatus status='${testBuildStatus.failed}' text='${testBuildFailedReason}']`);
+        });
       });
     });
   });
