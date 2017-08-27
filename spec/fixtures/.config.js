23dca5d085b926695f2aa533226409e9afaad253
akaguny <alexeykolpino@gmail.com>
2017-08-27 14:29:14 +0300

    test(*): фикстура для файла конфигурации

---
+++ spec/fixtures/.config.js
@@ -0,0 +1,12 @@
+module.export = {
+    eslint: {
+        current: 'current',
+        master: '',
+        result: ''
+    },
+    teamcity: {
+        login: 'login',
+        pass: 'pass',
+        host: 'host'
+    }
+}
