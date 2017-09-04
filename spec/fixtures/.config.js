98261d8d17ea0dccfa2cf85efd0a3b3eb83462e6
akaguny <alexeykolpino@gmail.com>
2017-09-04 17:33:15 +0300

    feature(*): config

--- spec/fixtures/.config.js
+++ spec/fixtures/.config.js
@@ -1,13 +1,12 @@
-module.export = {
-    eslint: {
-        current: 'pathTocurrent',
-        master: 'pathTomaster',
-        result: 'pathToResult'
-    },
-    teamcity: {
-        login: 'testLogin',
-        pass: 'testPass',
-        host: 'testHost',
-        projectId: 'testProjectId'
-    }
-}
+module.export = config;
+
+config.eslint = {
+  currentJson: `/home/alexey/IdeaProjects/sfa/toSFA/toApache/htdocs/ps/reports/eslint-report.json`,
+  masterJSON: `/home/alexey/Documents/IdeaProjects/eslint-teamcity-failed-conditions/spec/fixtures/error.json`
+};
+config.teamcity = {
+  login: '***REMOVED***',
+  password: '***REMOVED***',
+  host: 'https://***REMOVED***',
+  projectId: '***REMOVED***'
+};
